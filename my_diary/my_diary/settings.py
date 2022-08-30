2a2107e1a83cdea2b6b25da43ac1af3e132eea2d
Zeef0 <79386646+Zeef0@users.noreply.github.com>
2022-08-30 19:18:00 +0800

    sussy mommy

--- my_diary/my_diary/settings.py
+++ my_diary/my_diary/settings.py
@@ -130,12 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = "media/"
-
 STATIC_DIRS = [
     os.path.join(BASE_DIR, "static/")
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "media/"
+
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 TAILWIND_APP_NAME = 'theme'
