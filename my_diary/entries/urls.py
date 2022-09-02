c7a58ff8a2e8f97afd706b6ac2fd9dea8ffadcff
Zeef0 <79386646+Zeef0@users.noreply.github.com>
2022-09-02 11:34:11 +0800

    created update view on entry view with permission on it, and started enhancing the query on entries/view.py. Also reset the front end because it looks bad

--- my_diary/entries/urls.py
+++ my_diary/entries/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path("entry/<slug:slug>", views.EntryDetailView.as_view(), name="entry_detail"),
-    path("new/entry", views.CreateEntryView.as_view(), name="create_entry")
+    path("new/entry", views.CreateEntryView.as_view(), name="create_entry"),
+    path("update/entry/<slug:slug>", views.UpdateEntryView.as_view(), name="update_entry")
 ]
--- my_diary/entries/views.py
+++ my_diary/entries/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-
+from django.db.models import Q, F
 from .models import Entry, Comment
 
 from django.views.generic import (
@@ -17,10 +17,17 @@ class Home(ListView):
     model = Entry
     template_name = "entries/home.html"
     context_object_name = "context"
+    print("test")
 
 
+    
     def get_queryset(self):
-        qs = Entry.objects.all().filter(privacy="Public")
+
+        """
+        Show all items where entries are set to public 
+        """
+        qs = Entry.objects.filter(Q(privacy="Public")| Q(owner__pk= self.request.user.pk))
+        
         return qs
 
 
@@ -42,3 +49,17 @@ class CreateEntryView(LoginRequiredMixin, CreateView):
 
 
 
+class UpdateEntryView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Entry
+    fields = ["title", "privacy", "content"]
+
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.owner
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user.id == post.owner.id:
+            return True
+        return False
